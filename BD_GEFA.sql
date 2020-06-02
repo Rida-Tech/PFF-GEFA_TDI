43080e443c3ce96afec21ff4735e5146023b6799
Rida-Tech <59773663+Rida-Tech@users.noreply.github.com>
2020-06-02 01:57:54 +0100

    Add files via upload

---
+++ BD_GEFA.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE PFF_GEFA_TDI
+use PFF_GEFA_TDI
+
+create TABLE Login(id int primary key identity(1,1),Nom varchar(30),Password varchar(20))
+
+insert into Login(Nom,Password) values('RIDA','admin1')
+insert into Login(Nom,Password) values('SOUKAINA','admin2')
+insert into Login(Nom,Password) values('HAMZA','admin3')
+insert into Login(Nom,Password) values('ZINAB','admin4')
